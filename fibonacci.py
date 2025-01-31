4189b7cb4b182b444f72cc67a890d62b9fd98043
adiibaba239 <aditya.dadhich.239@gmail.com>
2025-01-31 13:24:49 +0530

    this is ml calss

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+print("program for printing fibonacci sum")
+sum=2
+e1=0
+e2=2
+e3=0
+n=4000000
+while e3<n:
+    e3 = 4 * e2 + e1
+    print(e3,e2,e1)
+    sum+=e3
+    e1=e2
+    e2=e3
+sum=sum -e3
+print("this is sum",sum)
